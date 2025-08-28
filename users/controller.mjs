3ae2007bbce7f48ae24b8daff5786b98e2a67e05
Harsh Maurya <mauryah535@gmail.com>
2025-08-28 08:21:04 -0400

    Add forgot password and reset password functionality to user controller and router

--- users/controller.mjs
+++ users/controller.mjs
@@ -1,9 +1,24 @@
 const signup = (req, res, next) => {
-  res.json({ msg: "signup done" });
+  // TODO: sanitation
+  // TODO: hash password
+  // TODO: write user in DB
+  // TODO: send account verification email
+  res.json({ msg: "signup is successful" });
 };
 
 const login = (req, res, next) => {
+  // TODO: find user by email from DB
+  // TODO: check is account verified
+  // TODO: match hased password
+  // TODO: Generate JWT Token
   res.json({ msg: "login done" });
 };
 
-export { signup, login };
+const forgotPassword = (req, res, next) => {
+  res.json({ msg: "forgot password" });
+};
+const resetPassword = (req, res, next) => {
+  res.json({ msg: "reset password" });
+};
+
+export { signup, login, forgotPassword, resetPassword };
--- users/router.mjs
+++ users/router.mjs
@@ -1,7 +1,11 @@
 import express from "express";
 const userRouter = express.Router();
-import { login, signup } from "./controller.mjs";
+import { forgotPassword, login, resetPassword, signup } from "./controller.mjs";
 
-userRouter.post("/signup", signup).post("/login", login);
+userRouter
+  .post("/signup", signup)
+  .post("/login", login)
+  .patch("/forgotPassword", forgotPassword)
+  .patch("/resetPassword", resetPassword);
 
 export default userRouter;
